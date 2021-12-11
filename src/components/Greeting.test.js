a62bee614b38a6eb8ce4d203d54f8e24558a6c30
Jamal Ahmed <1155jamalahmed@gmail.com>
2021-12-11 14:59:31 +0500

    Basic of unit testing and integration testing on React

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,49 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
+import Greeting from './Greeting';
+
+describe('Greeting Component', () => {
+  test('Renders hello world as a text', () => {
+    //Arrange
+    render(<Greeting />);
+  
+    //Act
+    // ... nothing
+  
+    // Assert
+    const helloWorldElement = screen.getByText('Hello world'); 
+    expect(helloWorldElement).toBeInTheDocument();
+  }); 
+
+  test('renders good to see you if the button is NOT clicked!',() => {
+    render(<Greeting />);
+    const outputElement = screen.getByText('good to see you', { exact: false }); 
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('render Changed if the button is clicked!', () => {
+    // arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    //assert 
+    const outputElement = screen.getByText('Changed!');
+    expect(outputElement).toBeInTheDocument();
+  });
+  test('Does not render "good to see you" if the button is clicked', () => {
+    // arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    //assert 
+    const outputElement = screen.queryByText('good to see you', { exact: false});
+    expect(outputElement).toBeNull();
+  });
+});
+
